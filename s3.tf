7ffd562ea0e58f8126261793bf34fc108b6ecc3d
adinarayana <adinarayanaduggempudi@gmail.com>
2025-11-15 20:36:04 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "adi14062001"
+  bucket = "adi1.monobucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -23,13 +23,7 @@ status = "Enabled"
 }
 }
 
-terraform {
-backend "s3" {
-region = "ap-south-1"
-bucket = "adi14062001"
-key = "prod/terraform.tfstate"
-}
-}
+
 
 
 
