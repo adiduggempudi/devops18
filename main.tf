0d113f53c0366580901f690ac1ae9bf59a7bd6b8
adinarayana <adinarayanaduggempudi@gmail.com>
2025-11-15 20:38:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_launch_template" "web_server_as" {
-    name = "myproject"
-    image_id           = "ami-03695d52f0d883f65"
+    name = "myproject1"
+    image_id           = "ami-03695d52f0d883f6"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t2.micro"
     key_name = "mykp"
